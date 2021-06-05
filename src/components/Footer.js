a4919bbcebfdc8391a9d723d072458a16d624b16
Suraj Bhosale <suraj.bhosale@vineti.com>
2021-06-05 20:58:24 +0530

    Some changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,16 +14,16 @@ function Footer() {
     <Container fluid className="footer">
       <Row>
         <Col md="4" className="footer-copywright">
-          <h3>Designed and Developed by SURAJ BHOSALE</h3>
+          <h3>Designed and Developed by Suraj Bhosale</h3>
         </Col>
         <Col md="4" className="footer-copywright">
-          <h3>Copyright © {year} SB</h3>
+          <h3>Copyright © {year} Suraj Bhosale</h3>
         </Col>
         <Col md="4" className="footer-body">
           <ul className="footer-icons">
             <li className="social-icons">
               <a
-                href="https://github.com/soumyajit4419"
+                href="https://github.com/SurajBTester"
                 style={{ color: "white" }}
               >
                 <AiFillGithub />
@@ -31,7 +31,7 @@ function Footer() {
             </li>
             <li className="social-icons">
               <a
-                href="https://twitter.com/Soumyajit4419"
+                href="https://twitter.com/Surajbh41524220"
                 style={{ color: "white" }}
               >
                 <AiOutlineTwitter />
@@ -47,7 +47,7 @@ function Footer() {
             </li>
             <li className="social-icons">
               <a
-                href="https://www.instagram.com/s.o.u.m.y.a_j.i.t/"
+                href="https://www.instagram.com/being_sirbhosale_/"
                 style={{ color: "white" }}
               >
                 <AiFillInstagram />
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -7,10 +7,11 @@ import Particle from "../Particle";
 import leaf from "../../Assets/Projects/leaf.png";
 import emotion from "../../Assets/Projects/emotion.jpeg";
 import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
+import chatify from "../../Assets/Projects/suraj.jpg";
 import suicide from "../../Assets/Projects/suicide.png";
 import algo from "../../Assets/Projects/algo.png";
 import plant from "../../Assets/Projects/plant.jpeg";
+import comingSoon from "../../Assets/Projects/comingsoon.png";
 
 function Projects() {
   return (
@@ -21,16 +22,16 @@ function Projects() {
           My Recent <strong className="purple">Works </strong>
         </h1>
         <p style={{ color: "white" }}>
-          Here are a few projects I've worked on recently.
+          Here are a few projects I will work.
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
               imgPath={chatify}
               isBlog={false}
-              title="Chatify"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              link="https://github.com/soumyajit4419/Chatify"
+              title="Sir Bhosale"
+              description="Coming Soon....."
+              link="https://www.pngkit.com/view/u2t4o0r5w7q8u2w7_coming-soon-link-coming-soon/"
             />
           </Col>
 
@@ -38,9 +39,9 @@ function Projects() {
             <ProjectCard
               imgPath={leaf}
               isBlog={false}
-              title="Plant AI"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              link="https://github.com/soumyajit4419/Plant_AI"
+              title="Coming Soon"
+              description="Coming Soon....."
+              link="https://www.pngkit.com/view/u2t4o0r5w7q8u2w7_coming-soon-link-coming-soon/"
             />
           </Col>
 
@@ -48,9 +49,9 @@ function Projects() {
             <ProjectCard
               imgPath={editor}
               isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              link="https://github.com/soumyajit4419/Editor.io"
+              title="Coming Soon"
+              description="Coming Soon......"
+              link="https://www.pngkit.com/view/u2t4o0r5w7q8u2w7_coming-soon-link-coming-soon/"
             />
           </Col>
 
@@ -58,9 +59,9 @@ function Projects() {
             <ProjectCard
               imgPath={suicide}
               isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              link="https://github.com/soumyajit4419/AI_For_Social_Good"
+              title="Coming Soon"
+              description="Coming Soon......."
+              link="https://www.pngkit.com/view/u2t4o0r5w7q8u2w7_coming-soon-link-coming-soon/"
             />
           </Col>
 
@@ -68,12 +69,22 @@ function Projects() {
             <ProjectCard
               imgPath={emotion}
               isBlog={false}
-              title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              link="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
+              title="Coming Soon"
+              description="Coming Soon......"
+              link="https://www.pngkit.com/view/u2t4o0r5w7q8u2w7_coming-soon-link-coming-soon/"
             />
           </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={comingSoon}
+              isBlog={false}
+              title="Coming Soon Try Module"
+              description="Coming Soon......"
+              link="https://www.pngkit.com/view/u2t4o0r5w7q8u2w7_coming-soon-link-coming-soon/"
+            />
+          </Col>
+
         </Row>
         <h1 className="project-heading">
           My Recent <strong className="purple">Blog </strong> Posts
